a51665e82febafe935f2a63c1e3bf956d005f297
nmc9x <kysutudongcom@gmail.com>
2024-10-02 06:17:04 +0700

    Update Lesson 28

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  //initialize the selected random user
+  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  selectedUser = signal(DUMMY_USERS[this.randomIndex]);
 
+  //use path to get the image
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); // use copmput from signal
+
+  //Create a function to select a random user
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    console.log('randomIndex', randomIndex);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
